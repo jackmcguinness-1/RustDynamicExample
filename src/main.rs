a71ba15914d2c26d3e02bb4b993bbae5ee64b221
jackmcguinness-1 <jack1mcguinness@gmail.com>
2022-10-09 18:16:33 +0100

    update to add an extra level of abstraction

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,34 @@
 
 fn main(){
+
     let spec_thing = MoreSpecificThing{
         base: BaseThing { data: 10 },
         extra_data: 69
     };
 
     let s = does_something_with_base(&spec_thing);
-
     println!("{:#?}", s);
-    let f = does_something_with_boxed_base(Box::new(spec_thing));
 
+    let f = does_something_with_boxed_base(Box::new(spec_thing));
     println!("{:#?}", f);
+
+    let game_specific_thing = GameSpecificThing{
+        base: MoreSpecificThing { 
+            base: BaseThing{
+                data: 1
+            }, 
+            extra_data: 2 
+        },
+        game_data: 3
+    };
+
+    let g = does_something_with_more_specific(&game_specific_thing);
+    println!("{}", g);
 }
 
 struct BaseThing{
     pub data: usize
 }
-
 
 trait Base{
     fn base(&self) -> &BaseThing;
@@ -39,10 +51,35 @@ impl Base for BaseThing{
     }
 }
 
+trait MoreSpecific{
+    fn base(&self) -> &MoreSpecificThing;
+}
+
+impl MoreSpecific for MoreSpecificThing{
+    fn base(&self) -> &MoreSpecificThing{
+        &self
+    }
+}
+
+struct GameSpecificThing{
+    base: MoreSpecificThing,
+    game_data: usize
+}
+
+impl MoreSpecific for GameSpecificThing{
+    fn base(&self) -> &MoreSpecificThing{
+        &self.base
+    }
+}
+
 fn does_something_with_base(x: &dyn Base) -> usize{
     x.base().data * 2
 }
 
 fn does_something_with_boxed_base(x: Box<dyn Base>) -> usize{
     x.base().data * 4
+}
+
+fn does_something_with_more_specific(x: &dyn MoreSpecific) -> usize{
+    x.base().extra_data * 69
 }
